6db0264cbe4388d680fe0b371c9305728563869a
Justin Tran <jhtran023@gmail.com>
2023-09-15 16:12:00 +1000

    Import preview of past projects

---
+++ script.js
@@ -0,0 +1,43 @@
+const carousel = document.querySelector(".projects-carousel"),
+firstImg = carousel.querySelectorAll("img")[0];
+arrowIcons = document.querySelectorAll(".wrapper i");
+
+let isDragStart = false, prevPageX, prevScrollLeft;
+let firstImgWidth = firstImg.clientWidth + 14;
+let scrollWidth = carousel.scrollWidth - carousel.clientWidth;
+
+const showHideIcons = () => {
+    arrowIcons[0].style.display = carousel.scrollLeft == 0 ? "none" : "block";
+    arrowIcons[1].style.display = carousel.scrollLeft >= scrollWidth ? "none" : "block";
+}
+
+arrowIcons.forEach(icon => {
+    icon.addEventListener("click", () => {
+        carousel.scrollLeft += icon.id == "left-arrow-project" ? -firstImgWidth : firstImgWidth;
+        setTimeout(() => showHideIcons(), 60);
+    });
+});
+
+const dragStart = (e) => {
+    isDragStart = true;
+    prevPageX = e.pageX;
+    prevScrollLeft = carousel.scrollLeft;
+}
+
+const dragging = (e) => {
+    if (!isDragStart) return;
+    e.preventDefault();
+    carousel.classList.add("dragging");
+    let positionDiff = e.pageX - prevPageX;
+    carousel.scrollLeft = prevScrollLeft - positionDiff;
+    showHideIcons();
+}
+
+const dragStop = () => {
+    isDragStart = false;
+    carousel.classList.remove("dragging");
+}
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
+carousel.addEventListener("mouseup", dragStop);
